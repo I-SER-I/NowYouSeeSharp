c4e5f453509af1c4ffc11a422954bc69625fd1f0
Sergey <I_SER_I@hotmail.com>
2020-11-11 14:10:26 +0300

    Added simple phone book project

---
+++ SimplePhoneBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using SimplePhoneBook.Core;
+
+namespace SimplePhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           UserInterface simplePhoneBook = new UserInterface();
+           simplePhoneBook.Start();
+        }
+    }
+}
