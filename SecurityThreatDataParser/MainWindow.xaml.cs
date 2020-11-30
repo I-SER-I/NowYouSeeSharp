e9f03952b8218df4067af73c2565efbfec5c6df8
Sergey <I_SER_I@hotmail.com>
2020-12-01 02:37:39 +0300

    feat: init base instance and create simple UI

---
+++ SecurityThreatDataParser/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecurityThreatDataParser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void ButtonDownload_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ButtonSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ButtonChangeViewMode_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ButtonPrev_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/Cat.cs
@@ -0,0 +1,14 @@
+﻿namespace WpfApp1
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Cat(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
